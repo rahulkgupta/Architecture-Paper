f2cf0241e89f81e2fff62efe7a45ead1bbd24850
Rahul Gupta <rahulgupta@ubuntu.(none)>
2012-04-04 18:06:13 -0700

    basic idea to show Michael

--- public/javascripts/drawings.js
+++ public/javascripts/drawings.js
@@ -20,30 +20,30 @@ $("#sq2").addLayer({
 	fromCenter: false
 })
 
-// $("#sq3").addLayer({
-// 	method: "drawRect",
-// 	strokeStyle: 'black',
-// 	strokeWidth: 10,
-// 	fillStyle: "#ffffff",
-// 	x: 6, y: 6,
-// 	width: 64,
-// 	height: 95,
-// 	fromCenter: false
-// })
+$("#sq3").addLayer({
+	method: "drawRect",
+	strokeStyle: 'black',
+	strokeWidth: 10,
+	fillStyle: "#ffffff",
+	x: 6, y: 6,
+	width: 64,
+	height: 95,
+	fromCenter: false
+})
 
 
-// $("#sq4").addLayer({
-// 	method: "drawRect",
-// 	strokeStyle: 'black',
-// 	strokeWidth: 10,
-// 	fillStyle: "#ffffff",
-// 	x: 6, y: 6,
-// 	width: 64,
-// 	height: 95,
-// 	fromCenter: false
-// })
+$("#sq4").addLayer({
+	method: "drawRect",
+	strokeStyle: 'black',
+	strokeWidth: 10,
+	fillStyle: "#ffffff",
+	x: 6, y: 6,
+	width: 64,
+	height: 95,
+	fromCenter: false
+})
 
 $('#sq1').drawLayers();
 $('#sq2').drawLayers();
-// $('#sq3').drawLayers();
-// $('#sq4').drawLayers();
+$('#sq3').drawLayers();
+$('#sq4').drawLayers();
