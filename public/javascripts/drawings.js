8992bfcf7e7f437cf30a78ededcd38dc5c91c762
Rahul Gupta <rahulgupta@ubuntu.(none)>
2012-04-08 16:16:28 -0700

    work on getting text involved

--- public/javascripts/drawings.js
+++ public/javascripts/drawings.js
@@ -1,4 +1,7 @@
-
+// $('.carousel-inner').append('<div class="item active"><img src="http://www.jbdesign.it/idesignpro/images/minimalism/10172d1198944136t-tadao-ando-koshino-house-sketchup-cizimleri-andoexteriorppb-jpg.jpg"/></div>')
+// $('.carousel-inner').append('<div class="item"><img src="http://ad009cdnb.archdaily.net/wp-content/uploads/2011/08/1313721857-koshino8-mariana.jpg"/></div>')
+// $('.carousel-inner').append('<div class="item"><img src="http://ad009cdnb.archdaily.net/wp-content/uploads/2011/08/1313721868-koshino13-gonzalo-875x1000.jpg"/></div>')
+// $('.carousel').carousel();
 $("#rietveld-sq").addLayer({
 	method: "drawRect",
 	strokeStyle: '#080808',
@@ -174,30 +177,34 @@ $('#rietveld-sq').click(function(event) {
 $('#ando-sq').click(function(event) {
 	if ($('#ando-sq').attr('data-init') == 'true') {
 		$('#rietveld-sq').hide()
-		$('#sim-sq').hide();
 		$('body').css('background','#111111')
-		$('#ando-sq').css('left', '100px')
-		$('#ando-sq').animate({
-			top: "+=200px"}
-			,1000,function() {
-				$('.light').show();
-			  $('#ando-sq').attr('data-init', 'false')
+		$('#ando-sq').css('left', '740px')
+		$('.light').show();
+		$('#ando-info').show();
+		$('#ando-sq').attr('data-init', 'false')
+		// $('#ando-sq').animate({
+		// 	top: "+=200px"}
+		// 	,1000,function() {
+			
 			  
 			 
-			});
+			// });
 	} else {
 		$('.light').hide();
+		$('#ando-info').hide();
 		$('body').css('background','white')
-		$('#ando-sq').animate({
-			top: "-=200px"}
-			,1000,function() {
+		$('#rietveld-sq').show()
+		$('#ando-sq').css('left', '540px')
+		$('#ando-sq').attr('data-init', 'true')
+		// $('#ando-sq').animate({
+		// 	top: "-=200px"}
+		// 	,1000,function() {
 				
-				$('#ando-sq').css('left', '-100px')
-			  $('#ando-sq').attr('data-init', 'true')
-			  $('#rietveld-sq').show()
-				$('#sim-sq').show();
+				
+			  
+		// 		$('#sim-sq').show();
 			 
-			});
+		// 	});
 	}
 	
 })
@@ -230,4 +237,10 @@ $('#ando-4').hover(function(event) {
 	$('#ando-4-text').text('Structure & Form')
 }, function(event) {
 	$('#ando-4-text').text('')
+})
+
+
+$('#ando-1').click(function(event) {
+	$("#ando-text").text('Tadao Ando is famous for using concrete in his works. Hence, the Koshino House is built primarily of concrete with six exposed holes for board screws. The concrete is finely coated to create a smooth texture and to add luminosity, contradicting the hardness and roughness of concrete and giving the illusion of the building having a soft and textile surface. Like the Schroder House, the primary purpose of the concrete is to establish space, and the concrete is “ hard and carefully placed” (Details 48) in order to prevent cracks that could lead to the absorption of water. Besides concrete, glass is extensively used to provide natural sunlight to the interior. In fact, one can argue that sunlight itself is a material used in the construction and design of the Kohsino House, as it is an essential component that is experienced by the residents of the home.')
+	$('#ando-text').show();
 })
