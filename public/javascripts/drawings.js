4ac17f6d74c9e53d644a505a1307acb7a839c0f2
Rahul Gupta <rahulgupta@ubuntu.(none)>
2012-04-09 17:44:18 -0700

    did work on the front page, added pics

--- public/javascripts/drawings.js
+++ public/javascripts/drawings.js
@@ -4,6 +4,7 @@
 // $('.carousel').carousel();
 
 $("a[rel^='prettyPhoto']").prettyPhoto();
+$("a[rel^='rprettyPhoto']").prettyPhoto();
 
 $("#rietveld-sq").addLayer({
 	method: "drawRect",
@@ -120,6 +121,8 @@ $('#rietveld-sq').click(function(event) {
 		$('#rietveld-sq')
 		.animate({"top": "+=200px"});
 		$('#ando-sq').hide()
+		$('#thesis').hide()
+		$('#title').hide()
 		$('#sim-sq').hide();
 		$('#sq1-container').animate({
 	    left: '+=1200',
@@ -129,6 +132,9 @@ $('#rietveld-sq').click(function(event) {
 	  $('#sq2-container').animate({
 	    left: '-=3214',
 	  }, 1000, function() {
+	  	$('#rietveld-text').text('The Rietveld Schroder House was designed in 1922 and built in 1924 in Utrecht, Netherlands. While Gerrit Rietveld was the “architect” of the house and Mrs. Truus Schroder was the “client,” both individuals were critical to the design of the building. In fact, the two kindled a relationship, and Rietveld ended up living in the house with Schroder. The house was initially commissioned by the recently widowed Schroder, who desired to live in a house smaller than the one she had previously lived in. The house was designed with a focus on De Stijl and practicality, making it a marvel of the modern era.')
+	  	$('#rietveld-header').text('Main')
+	  	$('#rietveld-info').show();
 	    // Animation complete.
 	  });
 	  $('#sq3-container').animate({
@@ -141,14 +147,17 @@ $('#rietveld-sq').click(function(event) {
 	  }, 1000, function() {
 	    // Animation complete.
 	  });
-	  $('#rietveld-info').show();
+	  
 	  $('#rietveld-sq').attr('data-init', 'false')
 	} else {
+		$('body').css('background','#ffffff')
 		$('#rietveld-sq').animate({
 			top: "-=200px"}
 			,1000,function() {
 				$('#ando-sq').show()
 				$('#sim-sq').show();
+				$('#thesis').show()
+				$('#title').show()
 			});
 
 		$('#sq1-container').animate({
@@ -181,6 +190,8 @@ $('#rietveld-sq').click(function(event) {
 $('#ando-sq').click(function(event) {
 	if ($('#ando-sq').attr('data-init') == 'true') {
 		$('#rietveld-sq').hide()
+		$('#thesis').hide()
+		$('#title').hide()
 		$('body').css('background','#111111')
 		$('#ando-sq').css('left', '740px')
 		$('.light').show();
@@ -189,7 +200,10 @@ $('#ando-sq').click(function(event) {
 		// $('#ando-sq').animate({
 		// 	top: "+=200px"}
 		// 	,1000,function() {
-			
+		$('#ando-text').text('The Koshino House was designed by Tadao Ando, commissioned by the designer Koshin, and built in the early 1980s in Ashiya, Japan. Ando, a modernist, believed in individualism, an ideal opposite to the philosophies surround De Stijl. His individualism is seen in his work ethic, as assumes responsibility for his actions and goes through every detail of his firm’s designs personally. As seen by the design of the Koshino House, he is a modernist, during a time when everyone else has been swept by the Post Modern movement, showing Ando’s testament to individualism and aversion to conformity. A celebrated architect from Osaka, Ando is a, “Master of geometry and proportions that sustains a superb sense of proportion.” (Matsuba 9)')
+		$('#ando-1').attr('data-clicked', 'true')
+		$('#ando-1').css('background','#ffff99')
+		$('#ando-1-text').text('Main')
 			  
 			 
 			// });
@@ -198,14 +212,25 @@ $('#ando-sq').click(function(event) {
 		$('#ando-info').hide();
 		$('body').css('background','white')
 		$('#rietveld-sq').show()
+		$('#thesis').show()
+		$('#title').show()
 		$('#ando-sq').css('left', '540px')
 		$('#ando-sq').attr('data-init', 'true')
 		// $('#ando-sq').animate({
 		// 	top: "-=200px"}
 		// 	,1000,function() {
-				
-				
-			  
+		$('#ando-1').attr('data-clicked', 'false')
+		$('#ando-3').attr('data-clicked', 'false')
+		$('#ando-2').attr('data-clicked', 'false')
+		$('#ando-4').attr('data-clicked', 'false')		
+		$('#ando-1').css('background','#080808')
+		$('#ando-3').css('background','#080808')
+		$('#ando-2').css('background','#080808')
+		$('#ando-4').css('background','#080808')		
+		$('#ando-2-text').text('')
+		$('#ando-3-text').text('')
+		$('#ando-4-text').text('')
+		$('#ando-1-text').text('')
 		// 		$('#sim-sq').show();
 			 
 		// 	});
@@ -259,11 +284,14 @@ $('#ando-1').click(function(event) {
 	$('#ando-3').attr('data-clicked', 'false')
 	$('#ando-2').attr('data-clicked', 'false')
 	$('#ando-4').attr('data-clicked', 'false')
+	$('#ando-2-text').text('')
+	$('#ando-3-text').text('')
+	$('#ando-4-text').text('')
 	$('#ando-1').css('background','#ffff99')
 	$('#ando-3').css('background','#080808')
 	$('#ando-2').css('background','#080808')
 	$('#ando-4').css('background','#080808')
-	$("#ando-text").text('The Koshino House was designed by Tadao Ando, commissioned by the designer Koshin, and built in the early 1980s in Ashiya, Japan. Ando, a modernist, believed in individualism, an ideal opposite to the philosophies surround De Stijl. His individualism is seen in his work ethic, as assumes responsibility for his actions and goes through every detail of his firm’s designs personally. As seen by the design of the Koshino House, he is a modernist, during a time when everyone else has been swept by the Post Modern movement, showing Ando’s testament to individualism and aversion to conformity. A celebrated architect from Osaka, Ando is a, “Master of geometry and proportions that sustains a superb sense of proportion” (Matsuba 9)')
+	$("#ando-text").text('The Koshino House was designed by Tadao Ando, commissioned by the designer Koshin, and built in the early 1980s in Ashiya, Japan. Ando, a modernist, believed in individualism, an ideal opposite to the philosophies surround De Stijl. His individualism is seen in his work ethic, as assumes responsibility for his actions and goes through every detail of his firm’s designs personally. As seen by the design of the Koshino House, he is a modernist, during a time when everyone else has been swept by the Post Modern movement, showing Ando’s testament to individualism and aversion to conformity. A celebrated architect from Osaka, Ando is a, “Master of geometry and proportions that sustains a superb sense of proportion.” (Matsuba 9)')
 	$('#ando-text').show();
 })
 
@@ -273,6 +301,9 @@ $('#ando-2').click(function(event) {
 	$('#ando-1').attr('data-clicked', 'false')
 	$('#ando-4').attr('data-clicked', 'false')
 	$('#ando-2').css('background','#ffff99')
+	$('#ando-1-text').text('')
+	$('#ando-3-text').text('')
+	$('#ando-4-text').text('')
 	$('#ando-1').css('background','#080808')
 	$('#ando-3').css('background','#080808')
 	$('#ando-4').css('background','#080808')
@@ -283,6 +314,9 @@ $('#ando-2').click(function(event) {
 $('#ando-3').click(function(event) {
 	$('#ando-3').attr('data-clicked', 'true')
 	$('#ando-1').attr('data-clicked', 'false')
+	$('#ando-2-text').text('')
+	$('#ando-1-text').text('')
+	$('#ando-4-text').text('')
 	$('#ando-2').attr('data-clicked', 'false')
 	$('#ando-4').attr('data-clicked', 'false')
 	$('#ando-3').css('background','#ffff99')
@@ -299,6 +333,9 @@ $('#ando-4').click(function(event) {
 	$('#ando-1').attr('data-clicked', 'false')
 	$('#ando-2').attr('data-clicked', 'false')
 	$('#ando-3').attr('data-clicked', 'false')
+	$('#ando-2-text').text('')
+	$('#ando-3-text').text('')
+	$('#ando-1-text').text('')
 	$('#ando-4').css('background','#ffff99')
 	$('#ando-1').css('background','#080808')
 	$('#ando-2').css('background','#080808')
@@ -309,22 +346,26 @@ $('#ando-4').click(function(event) {
 
 
 $('#sq1-container').click(function(event) {
+	$('body').css('background','#ff2100')
 	$('#rietveld-header').text('Materials')
 	$('#rietveld-text').text('The materials of the Rietveld Schroder House were of great importance. While Rietveld wished to make the house entirely of concrete, a limited budget forced him to make house out of brick, for the walls; wood, for the window frames, doors, and floors; reinforced concrete, for the foundation and balconies; and steel, for the roof and columnal support. While Ando’s house seemed bare on the outside, Reitveld emulated the energy of the De Stijl by painting both the exterior and the interior with bold, mostly primary, colors such as red, white, black, blue, and yellow. Interestingly enough, the paint was applied with a focus on practicality, for black paint was used primarily on components, such as doors and handles, that would be susceptible to wear and tear.')
 })
 
 
 $('#sq2-container').click(function(event) {
+	$('body').css('background','#ff8900')
 	$('#rietveld-header').text('Interior')
 	$('#rietveld-text').text('Inside, the house is very practical, with an emphasis on space. Rietveld and Schroder believed, “that the size of the rooms must be related to the time spent in them” (Kuper, Zijl 98). At the same time, the two felt that rooms should not be strictly defined, which is exemplified in the second floor room organization. Rietveld designed the second story with thin, sliding walls which could be rearranged into a variety of configurations, such as three bedrooms and one sitting room. With sliding walls, Schroder could set up the bedrooms during the night, but get rid of the walls during the day to promote interaction amongst the residents, in line with the focus on universality. Unlike in Ando’s Koshino House, no space is used for corridors, preventing any form of hierarchy, and each room has access to the outside. Reitveld’s attention to practicality can be easily through the furniture design. Since Reitveld was primarily a furniture designer, he took it upon himself to implement furniture in such a way that it was an integral component of the home. For example, there are tables that can be folded to open up the room, and cupboards and storage areas are placed in a way to maximize space. In addition, the house contains highly innovative features such as a speaking tube, an early version of an intercom, and hot water pipes that would flow through certain locations to keep objects, such as shoes, warm. Overall, one can see Reitveld’s emphasis towards practicality nicely blended with his collectivist beliefs making the interior of the home a true innovation.')
 })
 
 $('#sq3-container').click(function(event) {
+	$('body').css('background','#ffffff')
 	$('#rietveld-header').text('Form')
 	$('#rietveld-text').text('Like the Koshino house, the structure of the Schroder house represents highly abstract geometry. After all, the facade of the two story house is composed of basic multiple intersecting planar elements, as if they all were moving in space and collided with one another to create such an overall shape. Each plane is interesting colored and unique in size and shape. However, when all these planar surfaces come together, the building as a whole represents something greater than simply the sum of the individual parts, showing the essence of the De Stijl movement and the focus on universality over individuality. Furthermore, the overall asymmetry properly represents the lack of focus on any one element and the general philosophies of the De Stijl movement. Such philosophy carries over to the positioning of the house, for it, “Deliberately faces away from Prins Hendriklaan, an avenue which Truus Scroder and Rietveld dismissed as ‘bourgeois’.” (Mulder, Zijl 8) Similar to the Koshino House, natural light plays an important role in the structure of the building, since there are many windows in the house, along with a skylight. In the end, the use of abstract geometry with a focus on collectivism controls the structure of the Rietveld-Schroder House.')
 })
 
 $('#sq4-container').click(function(event) {
+	$('body').css('background','#ffffff')
 	$('#rietveld-header').text('Main')
 	$('#rietveld-text').text('The Rietveld Schroder House was designed in 1922 and built in 1924 in Utrecht, Netherlands. While Gerrit Rietveld was the “architect” of the house and Mrs. Truus Schroder was the “client,” both individuals were critical to the design of the building. In fact, the two kindled a relationship, and Rietveld ended up living in the house with Schroder. The house was initially commissioned by the recently widowed Schroder, who desired to live in a house smaller than the one she had previously lived in. The house was designed with a focus on De Stijl and practicality, making it a marvel of the modern era.')
 })
